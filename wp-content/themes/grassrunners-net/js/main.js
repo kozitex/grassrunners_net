135290b624b6f8842441c976428b2a1e5b9efc9b
kozitex <kozitex@grassrunners.net>
2024-03-09 02:36:48 +0900

    20240308_1

--- wp-content/themes/grassrunners-net/js/main.js
+++ wp-content/themes/grassrunners-net/js/main.js
@@ -2,13 +2,35 @@
 
 window.addEventListener('DOMContentLoaded', function(){
 
+  var video = document.getElementById('video');
+  video.addEventListener('progress', function() {
+    console.log("progress");
+    stalker.classList.add('loading');
+  });
+
+  video.addEventListener('suspend', function() {
+    console.log("suspend");
+    stalker.classList.add('loading');
+  });
+
+  document.getElementById('video').addEventListener('loadeddata', function() {
+    console.log("loadeddata");
+    stalker.classList.remove('loading');
+  });
+
+  document.getElementById('video').addEventListener('canplay', function() {
+    console.log("canplay");
+    stalker.classList.remove('loading');
+  });
+
+
 	// スクロールに応じて、fvの文字を透明にする
 	window.addEventListener('scroll', function(){
 		var scrollAmt = window.scrollY;
 		var threshold = 1 / 1.5;
 		var winInHeight = window.innerHeight;
 		var opacity = 1 - scrollAmt / ( winInHeight * threshold );
-		this.document.getElementById('fv').style.opacity = opacity;
+		this.document.getElementById('mv').style.opacity = opacity;
 	});
 
 	// スクロールに応じて、videoの文字を透明にする
@@ -101,4 +123,63 @@ window.addEventListener('DOMContentLoaded', function(){
 			}
 		});
 	}
+
+  var
+  cursor = document.getElementById('cursor'),
+  stalker = document.getElementById('stalker'),
+  cursorSize = 16,
+  stalkerSize = 48,
+  mouseX = 0,
+  mouseY = 0,
+  amountX = 0,
+  amountY = 0;
+
+  window.addEventListener('mousemove', cursorMove);
+
+  // window.addEventListener('scroll', cursorMoveByScroll);
+
+  function cursorMove(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    console.log(mouseX, mouseY);
+    // cursor.style.left = mouseX - (cursorSize / 2) + "px";
+    // cursor.style.top  = mouseY - (cursorSize / 2) + "px";
+    // stalker.style.left = mouseX - (stalkerSize / 2) + "px";
+    // stalker.style.top  = mouseY - (stalkerSize / 2) + "px";
+    cursor.style.transform = 'translate(' + mouseX + 'px, ' + mouseY + 'px)';
+    stalker.style.transform = 'translate(' + mouseX + 'px, ' + mouseY + 'px)';
+  }
+
+  // function cursorMove(e) {
+  //   mouseX = e.pageX;
+  //   mouseY = e.pageY;
+  //   cursor.style.left = mouseX - (cursorSize / 2) + "px";
+  //   cursor.style.top  = mouseY - (cursorSize / 2) + "px";
+  //   // stalker.style.left = mouseX - (stalkerSize / 2) + "px";
+  //   // stalker.style.top  = mouseY - (stalkerSize / 2) + "px";
+  //   stalker.style.transform = 'translate(' + mouseX + 'px, ' + mouseY + 'px)';
+  // }
+
+  function cursorMoveByScroll() {
+    console.log(mouseX, mouseY);
+    var mouseX2 = mouseX + (window.scrollX - amountX);
+    var mouseY2 = mouseY + (window.scrollY - amountY);
+    console.log(mouseX2, mouseY2);
+    // mouseX = mouseX + window.scrollX;
+    // mouseY = mouseY + window.scrollY;
+
+
+    // cursor.style.left = mouseX - (cursorSize / 2) + "px";
+    // cursor.style.top  = mouseY - (cursorSize / 2) + "px";
+    // stalker.style.left = mouseX - (stalkerSize / 2) + "px";
+    // stalker.style.top  = mouseY - (stalkerSize / 2) + "px";
+    cursor.style.transform = 'translate(' + mouseX + 'px, ' + mouseY + 'px)';
+    stalker.style.transform = 'translate(' + mouseX + 'px, ' + mouseY + 'px)';
+    amountX = window.scrollX;
+    amountY = window.scrollY;
+  }
+
+
+
+  
 });
